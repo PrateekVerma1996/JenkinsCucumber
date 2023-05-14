7f1994493cce1f172d987960aa4c7df1ea11c8ca
PrateekVerma1996 <133569638+PrateekVerma1996@users.noreply.github.com>
2023-05-14 19:05:21 +0530

    Update TestRunnerJunit.java

--- cucumberProject/src/main/java/Runners/TestRunnerJunit.java
+++ cucumberProject/src/main/java/Runners/TestRunnerJunit.java
@@ -9,9 +9,7 @@ import org.junit.runner.RunWith;
         features = {"C:/Users/prate/Downloads/cucumberProject/cucumberProject/src/main/resources/features/hooks2.feature"}
         //dryRun = true
         ,glue = {"stepdefinitions"}
-         plugin = {"pretty","html:target/cucumber-html-report", "json:target/cucumber.json",
- "pretty:target/cucumber-pretty.txt","usage:target/cucumber-usage.json", 
-"junit:target/cucumber-results.xml","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+         plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
         ,monochrome = true,
 
         //tags = "@orateek",
